1d03df96138cde5a38cd525423bb4006c3bfc67a
Dustan Kasten <dustan.kasten@gmail.com>
2013-06-19 12:02:18 -0400

    Bump version. Release

--- dist/granger.js
+++ dist/granger.js
@@ -5,7 +5,7 @@
     __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
 
   Granger = (function() {
-    Granger.version = '0.1.0';
+    Granger.version = '0.1.1';
 
     function Granger(element, options) {
       var value;
